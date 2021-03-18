7f92db28bbea96fa6bea02ded45462f5c86314b1
oscarheimdahl <o.heimdahl@gmail.com>
2021-03-18 20:36:01 +0100

    done?

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'website';
+  emojiImages: string[] = [
+    'peace.png',
+    'poop.png',
+    'wave.png',
+    'heart-eyes.png',
+    'devil.png',
+    'heart.png',
+  ];
+  emojiIndex: number = 0;
+  emoji: string = this.emojiImages[0];
+  constructor() {
+    setInterval(() => {
+      this.emoji = this.emojiImages[this.emojiIndex];
+      this.emojiIndex++;
+      if (this.emojiIndex > this.emojiImages.length - 1) this.emojiIndex = 0;
+    }, 1000);
+  }
 }
